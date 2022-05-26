935e0788ae7feec34a8dcdc935a8c56b48453a9b
mairakhan1998 <munshat19@gmail.com>
2022-05-26 11:00:45 +0530

    first commit

---
+++ src/app/events/events.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { record } from './records';
+
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.css']
+})
+export class EventsComponent implements OnInit {
+
+  constructor() { }
+   public event:any = [{
+     id:1,
+     name: "event created",
+     place:"lucknow",
+     country:"india"
+   },
+   {
+    id:2,
+    name: " 2nd event created",
+    place:"Delhi",
+    country:"india"
+   },
+   {
+    id:3,
+    name: "3rd event created",
+    place:"lundon",
+    country:"UK"
+   }]
+
+  ngOnInit(): void {
+  }
+
+  public getEventBYId(id:number){
+    return record.filter(record => record.id ===id )
+    
+  }
+
+}
---
+++ src/app/events/records.ts
@@ -0,0 +1,9 @@
+let record = [
+    {'id':1 ,'code':'E00-81', 'name':'smith', 'department':'Accounting', 'salary':'INR 16000'},
+    {'id':2,'code':'E49-19', 'name':'vijay', 'department':'Developer', 'salary':'INR 30000'},
+    {'id':3,'code':'E13-34', 'name':'nitin', 'department':'Developer', 'salary':'INR 25000'},
+    {'id':4,'code':'E29-10', 'name':'kang', 'department':'Accounting', 'salary':'INR 22000'},
+    {'id':5,'code':'E29-18', 'name':'peter', 'department':'Testing', 'salary':'INR 22000'},
+];
+
+export {record}
